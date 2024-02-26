1ff936bd597c3a6d7e8347fa4f2452d4f4a17f55
IsrakAhmed <israkahmed7@gmail.com>
2024-02-26 22:58:02 +0600

    this file is assigned to Shahriar Hossen Jamil

--- accounts/insertAccount.php
+++ accounts/insertAccount.php
@@ -9,7 +9,7 @@ Access-Control-Allow-Methods, Authorization, X-Requested-With');
 global $db;
 require_once '../config.php';
 
-/* This File Is Assigned To Bakhtiar Sahib Rizvi : @BakhtiarSahib */
+/* This File Is Assigned To Shahriar Hossen Jamil : @ShahriarJamil8009 */
 
 // Create A New Account
 
