95f9027adc2bb5f814fe73357a92e7eddfc7c769
IsrakAhmed <israkahmed7@gmail.com>
2024-02-26 23:00:15 +0600

    this file is assigned to Shahriar Hossen Jamil

--- transactions/showAllTransactions.php
+++ transactions/showAllTransactions.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 global $db;
 require_once '../config.php';
 
-/* This File Is Assigned To Esrat Jahan Riya : @EsratRiya */
+/* This File Is Assigned To Shahriar Hossen Jamil : @ShahriarJamil8009 */
 
 // Show All Transactions In The Database
 
