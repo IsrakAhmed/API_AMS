0da63d6982721ae17bf6dc7e0e565b6386d9cab6
SUMAIYA <sumaiya20001234@gmail.com>
2024-02-27 23:19:48 +0600

    showAllAccounts.php & deleteAccount.php is successfully done

--- accounts/showAllAccounts.php
+++ accounts/showAllAccounts.php
@@ -13,6 +13,19 @@ require_once '../config.php';
 
 // Start Writing Your Code From Here
 
+$sql = "SELECT * FROM accounts";
+
+$result = mysqli_query($db, $sql) or die("SQL Query Failed.");
+
+if(mysqli_num_rows($result) > 0){
+
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+}
+else{
+    echo json_encode(array('message' => 'NO Record Found.','status' => false));
+}
+
 
 
 ?>
