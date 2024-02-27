36c9119929334d676f6ba59c247dc720c8312d85
EsratRiya <esratjahanriya1201@gmail.com>
2024-02-27 17:29:27 +0600

    Done By Riya

--- accounts/showOneAccount.php
+++ accounts/showOneAccount.php
@@ -11,8 +11,20 @@ require_once '../config.php';
 // Show One Account In The Database
 
 
-// Start Writing Your Code From Here
+$data = json_decode(file_get_contents("php://input"), true);
+$account_id = $data['account_id'];
 
+$sql = "SELECT * FROM accounts WHERE account_id = {$account_id}";
+
+$result = mysqli_query($db, $sql) or die("SQL Query Failed.");
+
+if(mysqli_num_rows($result) > 0){
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+}
+else{
+   echo json_encode(array('message' => 'No Record Found.','status' => false));
+}
 
 
 ?>
