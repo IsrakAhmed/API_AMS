87fd4304fd2344ca8263dcf7805433398dd57421
IsrakAhmed <israkahmed7@gmail.com>
2024-02-28 02:19:20 +0600

    done by : ShahriarJamil

--- users/deleteUser.php
+++ users/deleteUser.php
@@ -13,11 +13,25 @@ require_once '../config.php';
 // Delete a user
 
 $data = json_decode(file_get_contents('php://input'), true);
-$student_id = $data['userid'];
-$sql = "DELETE FROM users WHERE userid = {$student_id}";
+$userid = $data['userid'];
+
+$stmt = $db->prepare("SELECT * FROM users WHERE userid = ?");
+$stmt->bind_param("i", $userid);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+
+if(!$row)
+{
+    echo json_encode(array('message' => 'No record found.', 'status' => false));
+    exit();
+}
+
+$sql = "DELETE FROM users WHERE userid = {$userid}";
+
 if(mysqli_query($db, $sql))
 {
-    echo json_encode(array('message' => 'Student record deleted.', 'status' => true));
+    echo json_encode(array('message' => 'User record deleted.', 'status' => true));
 }
 else
 {
